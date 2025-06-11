fc27d30b8a2d98d19f11b95ff1a77c99fc437d84
dev-code-create <ayushkankale04@gmail.com>
2025-06-11 23:49:48 +0530

    binarytree

---
+++ Binary Trees/binaryTree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = right = NULL;
+  }
+};
+int main()
+{
+
+  return 0;
+}
