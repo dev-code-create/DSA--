47a6e176d12cd67e2c68f34e73d7730594ef52e4
dev-code-create <ayushkankale04@gmail.com>
2025-08-03 03:02:13 +0530

    Create prob.cpp

---
+++ Hashmap & Hashset/Anagram/prob.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> groupAnagramsBetter(vector<string> strs)
+{
+  unordered_map<string, vector<string>> mp;
+  for (string s : strs)
+  {
+    string key = s;
+    sort(key.begin(), key.end());
+    mp[key].push_back(s);
+  }
+
+  vector<vector<string>> result;
+  for (auto &pair : mp)
+  {
+    result.push_back(pair.second);
+  }
+
+  return result;
+}
