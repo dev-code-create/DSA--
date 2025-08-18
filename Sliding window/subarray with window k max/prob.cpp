e74873cfe5b479263f1e138eafc5ab9ecc047cc6
dev-code-create <ayushkankale04@gmail.com>
2025-08-18 23:45:36 +0530

    Update prob.cpp

--- Sliding window/subarray with window k max/prob.cpp
+++ Sliding window/subarray with window k max/prob.cpp
@@ -5,7 +5,7 @@ int maxSum_slidingWindow(vector<int> &arr, int k)
 {
   int n = arr.size();
   if (n < k)
-    return -1; // invalid case
+    return -1; // invalid casee
 
   int windowSum = 0;
   for (int i = 0; i < k; i++)
